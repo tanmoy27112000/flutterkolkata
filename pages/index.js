ef7790f05dca47db2a2c3b03ff40c5882f4c9eec
Sebabrata Kundu <sebabratakundu9@gmail.com>
2023-07-02 14:01:46 +0530

    fix: move base layout to root (_app)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {BaseLayout, Hero, Showcase, UpcomingEvents} from "../components";
+import {Hero, Showcase, UpcomingEvents} from "../components";
 // import Container from "@mui/material/Container";
 // import Typography from "@mui/material/Typography";
 // import Box from "@mui/material/Box";
@@ -10,12 +10,10 @@ import {BaseLayout, Hero, Showcase, UpcomingEvents} from "../components";
 export default function Index() {
   return (
     <>
-      <BaseLayout>
-        <Hero />
-        <UpcomingEvents />
-        <Showcase type="poweredBy" />
-        <Showcase type="communityPartners" />
-      </BaseLayout>
+      <Hero />
+      <UpcomingEvents />
+      <Showcase type="poweredBy" />
+      <Showcase type="communityPartners" />
     </>
   );
 }
